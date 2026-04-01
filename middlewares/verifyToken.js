503ef0663d93817ae10d8b3da1bbb68abce485bd
MaricieloPeralta <peraltacielito@gmail.com>
2026-04-01 09:05:08 -0500

    status error

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -5,14 +5,15 @@ function verifyToken(req, res, next) {
     try {
         const authHeader = req.headers['authorization'];
         if (!authHeader) {
-            return res.status(401).json({ error: 'Acceso denegado' });}
+            return res.status(401).json({ error: 'Acceso denegado' });
+        }
         const token = authHeader.split(' ')[1];
         const decoded = jwt.verify(token, jwtSecret);
         req.usuario = decoded;
         next();
-        }
+    }
     catch (error) {
-        return res.status(401).json({ error: 'Token inválido o expirado :c!!!' });
+        return res.status(500).json({ error: 'Token inválido o expirado :c!!!' });
     }
 }
 module.exports = verifyToken;
